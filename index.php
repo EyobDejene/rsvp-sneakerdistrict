4abbdb6a5dc5027af52d32391844f9cb38c9b753
eyobdejene <eyob_westerink@hotmail.com>
2019-04-03 16:53:33 +0200

    changed metadata

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" type="text/css" href="assets/css/reset.css"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
     <meta name="msapplication-tap-highlight" content="no" />
-    <link rel="canonical" href="https://rsvp.sneakerdistrict.com">
+    <link rel="canonical" href="https://bus.sneakerdistrict.com">
 
     <meta name="author" content="Sneaker District">
     <meta name="copyright" content="Sneaker District. Copyright (c) <?php echo date("Y"); ?>"/>
@@ -16,8 +16,8 @@
     <meta property='og:title' content='Sneaker District webshop for sneakers & apparel'/>
     <meta property='og:description' content='Our Antwerp store is turning 1 year old! We would like to invite you to celebrate this with us. There will be free drinks by Jack Daniel’s and great tunes by MOM Antwerp. Adidas will also release the new Nite Jogger event, so make sure to be there!'/>
     <meta property='og:url' content='https://rsvp.sneakerdistrict.com'/>
-    <meta property='og:site_name' content='Sneaker District RSVP'/>
-    <meta property='og:image' itemprop='image' content='https://rsvp.sneakerdistrict.com/assets/images/share-image.jpg'/>
+    <meta property='og:site_name' content='Sneaker District Bus RSVP'/>
+    <meta property='og:image' itemprop='image' content='https://bus.sneakerdistrict.com/assets/images/share-image.jpg'/>
     <meta property="og:image:width" content="1200" />
     <meta property="og:image:height" content="630" />
     <meta property='og:type' content='website' />
@@ -29,9 +29,9 @@
     <meta name="twitter:description" content="Sneaker District Antwerp is turning 1 and we’ve set the date to
     celebrate this with you! On the 11th of April. On this very night we also release a new model of Adidas, the Nite
      Jogger.">
-    <meta name="twitter:image:src" content="https://rsvp.sneakerdistrict.com/assets/images/share-image.jpg"/>
+    <meta name="twitter:image:src" content="https://bus.sneakerdistrict.com/assets/images/share-image.jpg"/>
 
-    <link rel="icon" href="https://rsvp.sneakerdistrict.com/assets/images/favicon.png" type="image/png" />
+    <link rel="icon" href="https://bus.sneakerdistrict.com/assets/images/favicon.png" type="image/png" />
 
 </head>
 <body>
